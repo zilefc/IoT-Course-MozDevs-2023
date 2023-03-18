cede5c99b5835a22ac688cffd574fda1f4c506ac
Frederico Zile <46302322+zilefc@users.noreply.github.com>
2023-03-18 05:55:51 +0200

    Add files via upload

---
+++ FireBase+Arduino+Web/aula_2-3_firebase/aula_2-3_firebase.ino
@@ -0,0 +1,51 @@
+#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include <FirebaseArduino.h>
+#include <ArduinoJson.h>
+#include <ESP8266HTTPClient.h>
+
+#define FIREBASE_HOST ""
+#define FIREBASE_AUTH ""
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+const int Sala = 2;
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(Sala, OUTPUT);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected: ");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  if (Firebase.failed())
+  {
+    Serial.print(Firebase.error());
+  }
+  else {
+    Serial.print("Firebase Connected");
+  }
+
+
+}
+void loop()
+{
+  
+  String LuzSala = Firebase.getString("/SALA/estado");
+
+  if (LuzSala == "LIGADA") {
+    digitalWrite(Sala, LOW);
+  }
+  else if (LuzSala == "DESLIGADA") {
+    digitalWrite(Sala, HIGH);
+  }
+}
